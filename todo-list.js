5fdcb69931a944a78b04b13d9ac833d5f0a24f3c
David Gregorio Fuentes-Mendez <dgreegmdz@hotmail.com>
2025-07-09 14:05:52 -0600

    Added 'todo-list.js' which has a list of requirements to tackle in the following update. Will be done via Codex

---
+++ todo-list.js
@@ -0,0 +1,7 @@
+// TODO: Add optional long break after N cycles
+// Requirements:
+//   • User sets longBreakMin and longBreakEvery in settings.
+//   • After every N completed work blocks, insert a long break instead of a short one.
+//   • Sounds: play "break" at the start, but distinguish in notification title.
+//   • UI: show remaining long breaks in the popup (optional).
+
