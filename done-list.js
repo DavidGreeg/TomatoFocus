3977cc7c9c04ae2896a8c6c5dfee4076e4f47a83
David Gregorio Fuentes-Mendez <dgreegmdz@hotmail.com>
2025-07-10 14:59:52 -0600

    Finished the optional longbreak settings

---
+++ done-list.js
@@ -0,0 +1,9 @@
+// DONE: Added optional longbreak arguments after N cycles
+// Functionalities:
+//   • User can set longbreak minutes and number of cycles in settings.
+//   • Checkbox to activate/deactivate longbreak arguments.
+//   • Description: after every N completed work blocks,
+//                  inserts a long break instead of a short one.
+//   • Sounds: 
+//     - Play "break" and then "longbreak", distinguish in notification title.
+//     - Added 'playSeq' function in 'service_worker' to play both sounds.
